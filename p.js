e7c261d501396ad5cbaeb707d7956d31d3b91dbf
dinhmv <dinhmv@live.com>
2021-05-15 10:32:47 +0700

    Update p.js

--- p.js
+++ p.js
@@ -4,7 +4,7 @@ var BLACK = "PROXY 127.0.0.1:8021";
 var WHITE = PROXY_DIRECT;
 
 function FindProxyForURL(url, host) {
-  if (dnsDomainIs(host, "example.com")) {
+  if (dnsDomainIs(host, "vnexpress.net")) {
     return BLACK;
   }
   return PROXY_DIRECT;
